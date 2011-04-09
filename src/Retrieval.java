2ccb8d796b4846ab877a9c7113b0ef2e69b5d5e7
Benjamin Bachhuber <benbachhuber@gmail.com>
2011-04-09 13:00:29 +0200

    added main class

---
+++ src/Retrieval.java
@@ -0,0 +1,15 @@
+import java.io.File;
+
+public class Retrieval
+{
+	private File[] indicesUsed;
+	private File[] queryDocuments;
+	private int k;
+	private boolean similarityL1;
+	private boolean similarityL2;
+	
+	public static void main(String[] args)
+	{
+		
+	}
+}
